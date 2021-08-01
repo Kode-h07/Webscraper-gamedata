0c5b763cbb996f7b334252928a49cbe9144d25a7
jake <jakeko0912@gmail.com>
2021-08-02 02:13:22 +0900

    wrote csv file

--- save.py
+++ save.py
@@ -1,4 +1,9 @@
 import csv
 
 def save_to_file(players):
+    file = open('players.csv', mode='w', encoding='utf-8')
+    writer = csv.writer(file)
+    writer.writerow(['name', 'overall', 'position', 'link'])
+    for player in players:
+        writer.writerow(player.values())
     return
