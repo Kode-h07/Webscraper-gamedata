f803f7060d34082656f9720ab292ed400b358009
jake <jakeko0912@gmail.com>
2021-08-02 00:17:15 +0900

    extracting data from website

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 from bs4 import BeautifulSoup
+import requests
+
+def extract_data(html):
+    name = html.find('div', {'class': 'pcdisplay-name'}).text
+    overall = html.find('div', {'class': 'pcdisplay-rat'}).text
+    position = html.find('div', {'class': 'pcdisplay-pos'}).text
+    for a in html.find_all('a', href=True):
+        link = 'https://www.futbin.com/'+a['href']
+    return {
+        'name': name,
+        'overall': overall,
+        'position': position,
+        'link': link
+    }
+
+
+
+
+
+def extract_player():
+    players = []
+    result = requests.get("https://www.futbin.com/popular")
+    soup = BeautifulSoup(result.text, 'html.parser')
+    results = soup.find_all('li', {"class": 'popular-box'})
+    for result in results:
+        player = extract_data(result)
+        players.append(player)
+    return players
+
+print(extract_player())
+
 
